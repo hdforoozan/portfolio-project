fe6706fa4899ef19b2afec2d501e56f83166f7ad
Hadi Foroozanfar <hdforoozan@gmail.com>
2019-03-01 13:18:17 +0330

    Adding blog templates and complete the links

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def allblog(request):
+	blogs = Blog.objects
+	return render(request, 'blog/allblog.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+	blog = get_object_or_404(Blog,pk=blog_id)
+	return render(request, 'blog/detail.html', {'blog': blog})
